3271e21909e9a0a6bd233d890c7f9eaf51ac8654
AlisomLopes <alisomlopesps@gmail.com>
2024-04-29 14:09:49 -0300

    study...

--- main.js
+++
@@ -1,8 +0,0 @@
-const mobile_button = document.getElementById('mobile_button');
-const navigation = document.querySelector('.navigation_container');
-
-function handleClick() 
-{
-    mobile_button.classList.toggle('active');
-    navigation.classList.toggle('active');
-}
